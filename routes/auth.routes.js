d17898a8990c45b080df1755a86c2f305ffe3991
Ralf Bonnick <ralfbonnick@gmx.net>
2022-11-03 20:55:23 +0100

    the base is working - sessions and middleware to 90% done

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const router = express.Router();
 
+const { isLoggedIn, isLoggedOut } = require('../middleware/route-guard.js');
+
 // get Sign Up
 
-router.get('/signup', (req, res) => {
+router.get('/signup', isLoggedOut, (req, res) => {
+    console.log('SESSION =====> ', req.session)
     res.render('auth/signup')
   })
 
+  module.exports = router;
