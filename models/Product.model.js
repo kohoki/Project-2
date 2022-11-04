9ee04617a3225040b7983b2dd330fa3dd9cc23fd
Etienne Bellé <bl.etienne7@gmail.com>
2022-11-04 14:27:25 +0100

    create product schema & view

---
+++ models/Product.model.js
@@ -0,0 +1,34 @@
+const { Schema, model } = require("mongoose");
+
+// TODO: Please make sure you edit the User model to whatever makes sense in this case
+const productSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      unique: true
+    },
+    region: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: [String],
+    },
+    picture: {
+      type: String,
+    },
+    price: {
+      type: Number,
+      required: true
+    }
+  },
+  {
+    // this second object adds extra properties: `createdAt` and `updatedAt`    
+    timestamps: true
+  }
+);
+
+const Product = model("Product", productSchema);
+
+module.exports = Product;
